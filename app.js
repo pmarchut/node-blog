1437f3a18d91f1c602c5b1acc128f35953d062cf
Patryk Marchut <pm9701133@gmail.com>
2025-09-10 14:45:20 +0200

    [ADD] app: conntect to mongoDB with mongoose and handled /blogs route [ADD] created blog model [FIX] styles: nav responsive

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => app.listen(3000))
+  .catch(err => console.error(err));
+
 // register view engine
 app.set('view engine', 'ejs');
-
-// listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'How to defeat Bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' }
-    ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then(result => {
+            res.render('index', { title: 'All Blogs', blogs: result });
+        })
+        .catch(err => { 
+            console.error(err); 
+        });
 });
 
 app.get('/blogs/create', (req, res) => {
